0df7a2134bfedee7bab43f8bb9a936428243e81e
Basilio <basilio.uvinas@gmail.com>
2024-11-02 08:43:14 +0100

    Feat: Tailwind configuratons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+const forms = require('@tailwindcss/forms')
+const typography = require('@tailwindcss/typography')
+const aspectRatio = require('@tailwindcss/aspect-ratio')
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#1D4ED8', // Color principal
+          light: '#3B82F6', // Versión más clara
+          dark: '#1E40AF' // Versión más oscura
+        },
+        secondary: {
+          DEFAULT: '#D97706', // Color secundario
+          light: '#F59E0B', // Versión más clara
+          dark: '#B45309' // Versión más oscura
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: [forms, typography, aspectRatio]
 }
