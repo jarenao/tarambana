cb5aa763896c9ff12b03643ec2ebc1fd32b313a0
Basilio <basilio.uvinas@gmail.com>
2024-11-29 18:01:51 +0100

    Feat: Widget cloudinary

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Navbar from '../components/layout/Navbar.vue'
 import Footer from '../components/layout/Footer.vue'
+import UploadWidget from '../components/common/UploadWidget.vue'
 
 import { useI18n } from 'vue-i18n'
 
@@ -9,5 +10,8 @@ const { t } = useI18n()
 
 <template>
   <Navbar></Navbar>
+
+  <UploadWidget />
+
   <Footer></Footer>
 </template>
